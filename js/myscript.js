d44c669077637a6d786cc191bd0478c00b541cd0
vale <valemil.vs@gmail.com>
2020-08-26 16:46:15 +0200

    #3 e #4

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,13 @@
 // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome ed età.
 // Stampare a schermo attraverso il for in tutte le proprietà.
-// - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome.
-// - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+// - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti
+//   e stampare per ognuno nome e cognome.
+// - Dare la possibilità all’utente attraverso 3 prompt
+//   di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 // Usate prima i console.log e poi provare a stampare con jQuery:
 // BONUS: Provate ad utilizzare Handlebars.
 
+$(document).ready(function(){
 
 //#1: Creo oggetto con proprietà richieste:
 var studente = {
@@ -19,3 +22,47 @@ for (var k in studente) {
 }
 
 //#3: Creo array di oggetti di studenti:
+var studenti = [
+    {
+        'nome': 'Ina',
+        'cognome': 'Metaliu',
+        'eta': 29,
+    },
+    {
+        'nome': 'Pietro',
+        'cognome': 'Sette',
+        'eta': 37,
+    },
+    {
+        'nome': 'Martina',
+        'cognome': 'Fiore',
+        'eta': 25,
+    },
+    {
+        'nome': 'Alberto',
+        'cognome': 'Zordan',
+        'eta': 36,
+    },
+];
+
+//#3.1: Ciclo su tutti gli studenti e stampa nome e cognome:
+for (var i = 0; i < studenti.length; i++) {
+    console.log(studenti[i]);
+    for (var k in studenti) {
+        console.log(studenti[k].nome + ' ' + studenti[k].cognome);
+    }
+}
+
+//#4: 3 prompt per aggiunta oggetto studente da parte dell'utente:
+var nome = prompt('Inserisci un nome');
+var cognome = prompt('Inserisci un cognome');
+var eta = parseInt(prompt('Inserisci una età'));
+
+studenti.push({
+    'nome': nome,
+    'cognome': cognome,
+    'eta': eta,
+});
+console.log(studenti);
+
+});
